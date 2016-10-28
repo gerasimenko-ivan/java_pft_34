521bef29b676bafa7241be3525a100e452ec09cc
Ivan Gerasimenko <gistmp@gmail.com>
2016-10-28 20:38:33 +0300

    add Square & Rectangle classes

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,23 +6,23 @@ public class MyFirstProgram {
 		hello("John");
 		hello("user");
 
-		double l = 5;
-		System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
+		Square s = new Square(5);
+		System.out.println("Площадь квадрата со стороной " + s.l + " равна " + area(s));
 
-		double a = 4;
-		double b = 7;
-		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b));
+		Rectangle r = new Rectangle(4, 7);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + area(r));
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
 	}
 
-	public static double area(double l){
-		return l * l;
+	public static double area(Square s){
+		return s.l * s.l;
 	}
 
-	public static double area(double a, double b){
-		return a * b;
+	public static double area(Rectangle r){
+		return r.a * r.b;
 	}
+
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by gis on 28.10.2016.
+ */
+public class Square {
+    public double l;
+
+    public Square(double l) {
+        this.l = l;
+    }
+}
