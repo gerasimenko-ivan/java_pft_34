e26e2a8deda47da9e6fb8cbe04fb5c98c0f31a32
Ivan Gerasimenko <gistmp@gmail.com>
2016-10-28 20:28:58 +0300

    add several functions (simple calculations)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,27 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		hello("World");
+		hello("John");
+		hello("user");
+
+		double l = 5;
+		System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
+
+		double a = 4;
+		double b = 7;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b));
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area(double l){
+		return l * l;
+	}
+
+	public static double area(double a, double b){
+		return a * b;
 	}
 }
