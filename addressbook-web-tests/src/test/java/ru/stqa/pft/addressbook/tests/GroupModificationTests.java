4e88614dd4796668fdea7e11622e7264f69d1693
Ivan Gerasimenko <gistmp@gmail.com>
2016-11-07 07:33:36 +0300

    add GroupModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by gis on 07.11.2016.
+ */
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testGroupModification() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        GroupData groupData = new GroupData("new-test1", "new-test2", "new-test3");
+        app.getGroupHelper().fillGroupForm(groupData);
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
