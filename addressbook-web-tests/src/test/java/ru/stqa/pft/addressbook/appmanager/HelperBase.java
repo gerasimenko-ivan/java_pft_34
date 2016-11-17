a9241205e79ae64c2cc2429025dcf482397982ee
Ivan Gerasimenko <gistmp@gmail.com>
2016-11-17 21:40:40 +0300

    skip entering the same text into the field

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -23,8 +23,11 @@ public class HelperBase {
     protected void type(By locator, String text) {
         if (text != null) {
             WebElement element = wd.findElement(locator);
-            element.clear();
-            element.sendKeys(text);
+            String existingText = element.getAttribute("value");
+            if (! text.equals(existingText)) {
+                element.clear();
+                element.sendKeys(text);
+            }
         }
     }
 
