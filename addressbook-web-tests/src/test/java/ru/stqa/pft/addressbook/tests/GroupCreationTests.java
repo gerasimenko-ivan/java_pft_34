c82a7bd121b07544a6b400ea7a113f820c5d96f5
Ivan Gerasimenko <gistmp@gmail.com>
2016-11-22 06:45:48 +0300

    add group lists comparison to creation tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -15,11 +16,22 @@ public class GroupCreationTests extends TestBase {
 
         List<GroupData> groupsBefore = app.getGroupHelper().getGroupList();
 
-        GroupData groupData = new GroupData("test1", null, null);
-        app.getGroupHelper().createGroup(groupData);
+        GroupData group = new GroupData("test1", null, null);
+        app.getGroupHelper().createGroup(group);
 
         List<GroupData> groupsAfter = app.getGroupHelper().getGroupList();
         Assert.assertEquals(groupsAfter.size(), groupsBefore.size() + 1);
+
+
+        int max = 0;
+        for (GroupData groupItem : groupsAfter) {
+            if (groupItem.getId() > max) {
+                max = groupItem.getId();
+            }
+        }
+        group.setId(max);
+        groupsBefore.add(group);
+        Assert.assertEquals(new HashSet<Object>(groupsBefore), new HashSet<Object>(groupsAfter));
     }
 
 }
