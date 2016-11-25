be8e6765a34ea1c6978e736ebceb3ee76c7eb463
Ivan Gerasimenko <gistmp@gmail.com>
2016-11-26 02:00:39 +0300

    random name & surname functions

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/RandomDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/RandomDataGenerator.java
@@ -7,4 +7,14 @@ public class RandomDataGenerator {
     public int getInt(int min, int max) {
         return min + (int)(Math.random()*(max - min + 1));
     }
+
+    public String getFirstnameEng() {
+        String[] firstnames = {"Bob", "Pit", "Sam", "Ted", "Yen"};
+        return firstnames[getInt(0, firstnames.length - 1)];
+    }
+
+    public String getSurnameEng() {
+        String[] surnames = {"Davis", "Doe", "Lee", "Smith", "Tramp"};
+        return surnames[getInt(0, surnames.length - 1)];
+    }
 }
