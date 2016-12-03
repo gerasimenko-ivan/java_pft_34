a7fc9ef9b3ac8a6a3d2e967a900826f30c4bf626
Ivan Gerasimenko <gistmp@gmail.com>
2016-12-03 07:46:11 +0300

    add test testBadGroupCreation()

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -12,7 +12,6 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void testGroupCreation() {
         app.navigateTo().groupPage();
-
         Groups groupsBefore = app.group().all();
 
         GroupData group = new GroupData()
@@ -20,12 +19,26 @@ public class GroupCreationTests extends TestBase {
         app.group().create(group);
 
         // assertions
-
         Groups groupsAfter = app.group().all();
         assertThat(groupsAfter.size(), equalTo(groupsBefore.size() + 1));
 
         group.withId(groupsAfter.stream().mapToInt((g) -> g.getId()).max().getAsInt());
         assertThat(groupsAfter, equalTo(groupsBefore.withAdded(group)));
+    }
+
+    @Test
+    public void testBadGroupCreation() {
+        app.navigateTo().groupPage();
+        Groups groupsBefore = app.group().all();
+
+        GroupData group = new GroupData()
+                .withName("test'" + rnd.getInt(0, 1000));
+        app.group().create(group);
+
+        // assertions
+        Groups groupsAfter = app.group().all();
+        assertThat(groupsAfter.size(), equalTo(groupsBefore.size()));
+        assertThat(groupsAfter, equalTo(groupsBefore));
     }
 
 }
